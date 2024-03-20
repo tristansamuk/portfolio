eec565699c5967d72af4531d478546128b714d8c
Tristan Samuk <tsamuk@gmail.com>
2024-03-20 13:50:43 -0400

    Add basic dark theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import "./App.css";
+import "./styles/global.scss";
+import useLocalStorage from "use-local-storage";
 
 function App() {
-  return <></>;
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <div className="app" data-theme={theme}>
+      <h1>A Test of the Theme</h1>
+      <button onClick={switchTheme}>
+        Switch to {theme === "light" ? "dark" : "light"} theme
+      </button>
+    </div>
+  );
 }
 
 export default App;
