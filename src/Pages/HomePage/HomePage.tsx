da12fdbf40f42b3764f64e04505c6ca8bb420597
Tristan Samuk <tsamuk@gmail.com>
2024-03-20 19:24:48 -0400

    Create HomePage to test theme

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -5,9 +5,25 @@ type Props = {
 };
 
 const HomePage = ({ theme }: Props) => {
-  // switchTheme();
   return (
-    <div className="home-page__container--max-width" data-theme={theme}></div>
+    <div className="home-page__container--max-width" data-theme={theme}>
+      <section className="intro">
+        <h2 className="intro__heading">Hi, I'm Tristan</h2>
+        <p className="intro__text">
+          I'm a full-stack developer based in Toronto, Canada. I like building
+          software that's easy to use and nice to look at.
+        </p>
+        <p className="intro__text">
+          I'm currently working on expaning my portfolio and finding new
+          opportunties.
+        </p>
+        <p className="intro__text">
+          If you're looking to hire a junior developer or need help building
+          your website or app, feel free to reach out!
+        </p>
+        <button>Email Me</button>
+      </section>
+    </div>
   );
 };
 export default HomePage;
