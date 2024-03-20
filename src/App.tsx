11675a52d059c4be4e31db8abf741656726f4673
Tristan Samuk <tsamuk@gmail.com>
2024-03-20 14:38:20 -0400

    Finish basic and theme switch setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./styles/global.scss";
 import useLocalStorage from "use-local-storage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  // Checks and stores the value of `matches` property on the MediaQueryList object, which is true or false depending on the property passed into the `window.matchMedia()` function.
+
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [theme, setTheme] = useLocalStorage(
     "theme",
@@ -14,13 +17,19 @@ function App() {
   };
 
   return (
-    <div className="app" data-theme={theme}>
-      <h1>A Test of the Theme</h1>
-      <button onClick={switchTheme}>
-        Switch to {theme === "light" ? "dark" : "light"} theme
-      </button>
-    </div>
+    <BrowserRouter>
+      {/* Header component here */}
+      <Routes>{/* <Route path="/" element={<HomePage />}></Route> */}</Routes>
+      {/* Footer component here */}
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// <div className="app" data-theme={theme}>
+//   <h1>A Test of the Theme</h1>
+//   <button onClick={switchTheme}>
+//     Switch to {theme === "light" ? "dark" : "light"} theme
+//   </button>
+// </div>
