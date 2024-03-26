dbadfaeb899879ffd95f1fc6d3cf4a8bf6f6d5b8
Tristan Samuk <tsamuk@gmail.com>
2024-03-26 18:19:03 -0400

    Create cards for work section

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import "./Card.scss";
+
+const Card = () => {
+  return (
+    <div className="card__container">
+      <div className="card">
+        <img src="" alt="" />
+      </div>
+      <div className="card__container--text">
+        <h4 className="card__title">Close Reader</h4>
+        <p className="card__description">
+          An app for reading and learning about poetry with the help of AI
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,9 +1,15 @@
 import "./Work.scss";
+import Card from "../Card/Card";
+
+// type Props = {
+//   theme: string;
+// };
 
 const Work = () => {
   return (
     <div className="work__container--section">
       <h3 className="work__heading">Work</h3>
+      <Card />
     </div>
   );
 };
