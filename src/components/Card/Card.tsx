c507926be90e9f38c73f87063801076b0319e71d
Tristan Samuk <tsamuk@gmail.com>
2024-03-27 19:25:06 -0400

    Add test image to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,15 @@
 import "./Card.scss";
+import closeReaderMobile from "../../assets/images/closereader-mobile.png";
+
+// Note: pass in information about each project as prop from a json file
 
 const Card = () => {
   return (
-    <div className="card__container--grid">
+    <div className="card__container--flex">
       {/* Card 1 */}
       <div className="card__container">
         <div className="card">
-          <img src="" alt="" />
+          <img className="card__image" src={closeReaderMobile} alt="" />
         </div>
         <div className="card__container--text">
           <h4 className="card__title">Close Reader</h4>
