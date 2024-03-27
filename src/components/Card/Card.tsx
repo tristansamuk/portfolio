b3f5a81b012b960df999b00c2875da1766e135ab
Tristan Samuk <tsamuk@gmail.com>
2024-03-27 17:44:17 -0400

    Create grid layout for work examples

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,15 +2,42 @@ import "./Card.scss";
 
 const Card = () => {
   return (
-    <div className="card__container">
-      <div className="card">
-        <img src="" alt="" />
+    <div className="card__container--grid">
+      {/* Card 1 */}
+      <div className="card__container">
+        <div className="card">
+          <img src="" alt="" />
+        </div>
+        <div className="card__container--text">
+          <h4 className="card__title">Close Reader</h4>
+          <p className="card__description">
+            An app for reading and learning about poetry with the help of AI
+          </p>
+        </div>
       </div>
-      <div className="card__container--text">
-        <h4 className="card__title">Close Reader</h4>
-        <p className="card__description">
-          An app for reading and learning about poetry with the help of AI
-        </p>
+      {/* Card 2 */}
+      <div className="card__container">
+        <div className="card">
+          <img src="" alt="" />
+        </div>
+        <div className="card__container--text">
+          <h4 className="card__title">Close Reader</h4>
+          <p className="card__description">
+            An app for reading and learning about poetry with the help of AI
+          </p>
+        </div>
+      </div>
+      {/* Card 3 */}
+      <div className="card__container">
+        <div className="card">
+          <img src="" alt="" />
+        </div>
+        <div className="card__container--text">
+          <h4 className="card__title">Close Reader</h4>
+          <p className="card__description">
+            An app for reading and learning about poetry with the help of AI
+          </p>
+        </div>
       </div>
     </div>
   );
